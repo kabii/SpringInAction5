5b203b7724161fdbb4b01b4d9215d245af629854
kabii <lovekabi@gmail.com>
2020-07-24 01:26:12 +0900

    Ch01 gradle migration

--- Ch01/taco-cloud/src/main/java/tacos/HomeController.java
+++ Ch01/taco-cloud/src/main/java/tacos/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller                      // 컨트롤러                      
 public class HomeController {
 
-	@GetMapping("/")             // 루트 경로인 /의 웹 요청을 처리한다
-	  public String home() {
-	    return "home";           // 뷰 이름을 반환한다
-	  }
+    @GetMapping("/")             // 루트 경로인 /의 웹 요청을 처리한다
+    public String home() {
+        return "home";           // 뷰 이름을 반환한다
+    }
 
 }
